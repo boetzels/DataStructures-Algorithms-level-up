97adec43c096e4b0cacdbaa6d407c88ba34fff35
sboetzel <71010527+boetzels@users.noreply.github.com>
2022-02-09 23:50:18 +0100

    add recursive version and speed test of the 2 methods

--- Strings/SortSubsequences/src/Main.java
+++ Strings/SortSubsequences/src/Main.java
@@ -2,14 +2,26 @@ import java.util.*;
 
 public class Main {
 
+    private static ArrayList<String> subsequencesMain = new ArrayList<>();
+
     public static void main(String[] args) {
 	// write your code here
         Scanner sc = new Scanner(System.in);
         System.out.print("Give string: ");
         String text = sc.next();
 
-        ArrayList<String> sortedSubsequences = getAllSubsequences(text);
-        System.out.println(Arrays.asList(sortedSubsequences.toArray()));
+        long trainingTimeStartRec = System.currentTimeMillis();
+        getAllSubsequencesRec(text,"");
+        sortSubsequences(subsequencesMain);
+        long trainingTimeEndRec = System.currentTimeMillis();
+        System.out.println("Rec "+(trainingTimeEndRec-trainingTimeStartRec)+" ms benötigt");
+
+        long trainingTimeStart = System.currentTimeMillis();
+        ArrayList<String> sortedSubsequences = sortSubsequences(getAllSubsequences(text));
+        long trainingTimeEnd = System.currentTimeMillis();
+        System.out.println("Loop "+(trainingTimeEnd-trainingTimeStart)+" ms benötigt");
+
+        //System.out.println(Arrays.asList(sortedSubsequences.toArray()));
         System.out.println(sortedSubsequences.size());
     }
 
@@ -31,6 +43,23 @@ public class Main {
             }
         }
 
+        return subsequences;
+    }
+
+    private static void getAllSubsequencesRec(String text, String subsequence) {
+        if (text.isEmpty()) {
+            subsequencesMain.add(subsequence);
+            return;
+        }
+
+        String actualString = String.valueOf(text.charAt(0));
+        String newText = text.substring(1);
+
+        getAllSubsequencesRec(newText, subsequence.concat(actualString));
+        getAllSubsequencesRec(newText, subsequence);
+    }
+
+    private static ArrayList<String> sortSubsequences(ArrayList<String> subsequences) {
         Collections.sort(subsequences, (String s1, String s2) -> {
             if (s1.length() == s2.length()) {
                 return s1.compareTo(s2);
